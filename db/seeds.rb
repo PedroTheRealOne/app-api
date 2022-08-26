7b6eb656e559a75db5afe52076976e5f36945776
Pedro <pedrotherealone1@gmail.com>
2022-08-26 10:07:47 -0300

    feat: user and post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# USER seeds
+pedro = User.create(email: "pedrotherealone1@gmail.com", password: "123asd", password_confirmation: "123asd")
+marco = User.create(email: "marco@gmail.com", password: "123asd", password_confirmation: "123asd")
+jow = User.create(email: "jow@gmail.com", password: "123asd", password_confirmation: "123asd")
+
+# POST seeds
+post1 = Post.create(content: "FATE GO é muito slk", user: pedro)
+post2 = Post.create(content: "FATE GO é ruim dropa essa merda", user: jow)
+post3 = Post.create(content: "Rerun da hutao quando?", user: marco)
